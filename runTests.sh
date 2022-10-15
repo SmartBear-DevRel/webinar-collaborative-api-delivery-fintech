15ab843880c4384266f9516676a1c4b7b12d2064
Yousaf Nabi <yousaf.nabi@smartbear.com>
2022-10-15 10:56:13 +0100

    chore: remove server-stub

--- runTests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mkdir -p ${PWD}/ReadyAPI_Tests/reports \  && chmod ugo+rwx ${PWD}/ReadyAPI_Tests/reports &&
-    docker run --rm --network="host" \
-        -v=${PWD}/ReadyAPI_Tests:/ReadyAPI_Tests \
-        -e SLM_LICENSE_SERVER="https://api.slm.manage.smartbear.com:443" \
-        -e API_KEY=${SLM_API_KEY} \
-        -e ENDPOINT=${ENDPOINT} \
-        -e COMMAND_LINE="'-e${ENDPOINT}' '-f/ReadyAPI_Tests/reports' '-RJUnit-Style HTML Report' /ReadyAPI_Tests/SmartBearCoin-Payee-Provider-readyapi-project.xml" \
-        smartbear/ready-api-soapui-testrunner:latest
