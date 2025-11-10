07af68a743d6b5d975e50e741b0b013fda5e0755
Yousaf Nabi <you@saf.dev>
2025-11-10 20:10:48 +0000

    chore(test): migrate from jest to vitest

--- smartbearcoin-payments-ui/vite.config.js
+++ smartbearcoin-payments-ui/vite.config.js
@@ -1,15 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig(() => {
   return {
     build: {
-      outDir: 'build',
-      commonjsOptions: { transformMixedEsModules: true }
+      outDir: "build",
+      commonjsOptions: { transformMixedEsModules: true },
     },
     plugins: [react()],
     define: {
-        'process.env.VITE_APP_API_BASE_URL': process.env.VITE_APP_API_BASE_URL
-      }
+      "process.env.VITE_APP_API_BASE_URL": process.env.VITE_APP_API_BASE_URL,
+    },
+    test: {
+      globals: true,
+    },
   };
 });
