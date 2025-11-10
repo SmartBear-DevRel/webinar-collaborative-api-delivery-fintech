d173a7afc3d12e9d671af0de54bed7bce9fe215d
Yousaf Nabi <you@saf.dev>
2025-11-10 16:25:43 +0000

    test(ci): add pact verifications by pact broker selectors

--- provider_azure_function_tests/PactVerificationTest.cs
+++ provider_azure_function_tests/PactVerificationTest.cs
@@ -49,11 +49,83 @@ public class ProviderApiTests : IDisposable
         string providerName = "SmartBearCoin-Payee-Provider";
         IPactVerifier pactVerifier = new PactVerifier(providerName, config);
         string pactUrl = Environment.GetEnvironmentVariable("PACT_URL");
+        string pactFile = Environment.GetEnvironmentVariable("PACT_FILE");
+        string version = Environment.GetEnvironmentVariable("GIT_COMMIT");
+        string branch = Environment.GetEnvironmentVariable("GIT_BRANCH");
+        string buildUri = $"{Environment.GetEnvironmentVariable("GITHUB_SERVER_URL")}/{Environment.GetEnvironmentVariable("GITHUB_REPOSITORY")}/actions/runs/{Environment.GetEnvironmentVariable("GITHUB_RUN_ID")}";
 
-        pactVerifier.WithHttpEndpoint(new Uri(_providerUri))
-        .WithFileSource(new FileInfo(pactUrl))
-        .Verify();
 
+        if (pactFile != "" && pactFile != null)
+        // Verify a local file, provided by PACT_FILE, verification results are never published
+        // This step does not require a Pact Broker
+        {
+
+            pactVerifier.WithHttpEndpoint(new Uri(_providerUri))
+            .WithFileSource(new FileInfo(pactUrl))
+            .Verify();
+        }
+        else if (pactUrl != "" && pactUrl != null)
+        // Verify a remote file fetched from a pact broker, provided by PACT_URL, verification results may be published
+        // This step requires a Pact Broker
+        {
+            pactVerifier.WithHttpEndpoint(new Uri(_providerUri))
+            .WithUriSource(new Uri(pactUrl), options =>
+            {
+                if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("PACT_BROKER_TOKEN")))
+                {
+                    options.TokenAuthentication(Environment.GetEnvironmentVariable("PACT_BROKER_TOKEN"));
+                }
+                else if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("PACT_BROKER_USERNAME")))
+                {
+                    options.BasicAuthentication(Environment.GetEnvironmentVariable("PACT_BROKER_USERNAME"), Environment.GetEnvironmentVariable("PACT_BROKER_PASSWORD"));
+                }
+                options.PublishResults(!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("PACT_BROKER_PUBLISH_VERIFICATION_RESULTS")), version, results =>
+                    {
+                        results.ProviderBranch(branch)
+                        .BuildUri(new Uri(buildUri));
+                    });
+            })
+            .Verify();
+        }
+        else
+        {
+            // Verify remote pacts, provided by querying the Pact Broker via consumer version selectors, verification results may be published
+            // This step requires a Pact Broker
+            if (Environment.GetEnvironmentVariable("PACT_BROKER_BASE_URL") == null)
+            {
+                throw new InvalidOperationException("PACT_BROKER_BASE_URL environment variable is not set.");
+            }
+
+            pactVerifier.WithHttpEndpoint(new Uri(_providerUri))
+                .WithPactBrokerSource(new Uri(Environment.GetEnvironmentVariable("PACT_BROKER_BASE_URL")), options =>
+                {
+                    options.ConsumerVersionSelectors(
+                                new ConsumerVersionSelector { DeployedOrReleased = true },
+                                new ConsumerVersionSelector { MainBranch = true },
+                                new ConsumerVersionSelector { MatchingBranch = true }
+                            )
+                            .ProviderBranch(branch)
+                            .PublishResults(!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("PACT_BROKER_PUBLISH_VERIFICATION_RESULTS")), version, results =>
+                            {
+                                results.ProviderBranch(branch)
+                               .BuildUri(new Uri(buildUri));
+                            })
+                            .EnablePending()
+                            .IncludeWipPactsSince(new DateTime(2022, 1, 1));
+                    // Conditionally set authentication depending on if you are using an Pact Broker / PactFlow Broker
+                    // You may not have credentials with your own broker.
+                    if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("PACT_BROKER_TOKEN")))
+                    {
+                        options.TokenAuthentication(Environment.GetEnvironmentVariable("PACT_BROKER_TOKEN"));
+                    }
+                    else if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("PACT_BROKER_USERNAME")))
+                    {
+                        options.BasicAuthentication(Environment.GetEnvironmentVariable("PACT_BROKER_USERNAME"), Environment.GetEnvironmentVariable("PACT_BROKER_PASSWORD"));
+                    }
+
+                })
+                .Verify();
+        }
     }
 
     #region IDisposable Support
